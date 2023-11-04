feb63f31acbc223299e921f4bab613e50a02e663
Tien Dat <tiendatbinhan@gmail.com>
2023-11-04 22:54:35 +0700

    Change google-services.json to support new login method

--- app/src/main/java/com/nt118/uit21520707/jupita/LoginActivity.java
+++ app/src/main/java/com/nt118/uit21520707/jupita/LoginActivity.java
@@ -4,17 +4,30 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    private final FirebaseDatabase mUserDatabase = FirebaseDatabase.getInstance();
+
+    private final Button loginButton = findViewById(R.id.LG);
+
+    private final EditText editTextEmail = findViewById(R.id.edtUsn);
+
+    private final EditText editTextPassword = findViewById(R.id.edtPw);
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_logout);
 
-        mAuth = FirebaseAuth.getInstance();
+        loginButton.setOnClickListener(view -> {
+
+        });
     }
 }
