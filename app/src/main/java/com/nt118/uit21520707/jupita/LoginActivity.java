011b43193f899a149ad9090be18976b07a33eeb2
Tien Dat <tiendatbinhan@gmail.com>
2023-11-05 00:24:15 +0700

    Implement login

--- app/src/main/java/com/nt118/uit21520707/jupita/LoginActivity.java
+++ app/src/main/java/com/nt118/uit21520707/jupita/LoginActivity.java
@@ -1,33 +1,109 @@
 package com.nt118.uit21520707.jupita;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class LoginActivity extends AppCompatActivity {
     private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     private final FirebaseDatabase mUserDatabase = FirebaseDatabase.getInstance();
 
-    private final Button loginButton = findViewById(R.id.LG);
+    private Button loginButton;
 
-    private final EditText editTextEmail = findViewById(R.id.edtUsn);
+    private EditText editTextUsername;
 
-    private final EditText editTextPassword = findViewById(R.id.edtPw);
+    private EditText editTextPassword;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_logout);
 
+        loginButton = findViewById(R.id.LG);
+        editTextUsername = findViewById(R.id.edtUsn);
+        editTextPassword = findViewById(R.id.edtPw);
+
         loginButton.setOnClickListener(view -> {
 
+            final String targetUsername = editTextUsername.getText().toString();
+
+            Query userQuery = mUserDatabase.getReference("Users").orderByChild("username")
+                    .equalTo(targetUsername);
+
+            userQuery.addChildEventListener(new ChildEventListener() {
+                        String targetEmail;
+
+                        final String targetPassword = editTextPassword.getText().toString();
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                            User user = snapshot.getValue(User.class);
+                            if (user != null) {
+                                targetEmail = user.email;
+                                if (targetEmail == null) targetEmail = targetUsername + "@jupita.com";
+                            }
+                            else
+                            {
+                                Toast.makeText(LoginActivity.this,
+                                        "Wrong username or password. Try again.", Toast.LENGTH_LONG).show();
+                                Log.i("Login", "Login unsuccessfully");
+                                return;
+                            }
+
+                            mAuth.signInWithEmailAndPassword(targetEmail, targetPassword)
+                                    .addOnCompleteListener(task -> {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            Toast.makeText(LoginActivity.this,
+                                                    "Sign in successfully.", Toast.LENGTH_LONG).show();
+                                            Log.i("Login", "Login successfully");
+                                            finish();
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Toast.makeText(LoginActivity.this,
+                                                    "Wrong username or password. Try again.", Toast.LENGTH_LONG).show();
+                                            Log.i("Login", "Login unsuccessfully");
+                                        }
+                                    });
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
         });
     }
 }
