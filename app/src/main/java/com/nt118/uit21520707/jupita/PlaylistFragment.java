144b94f6e36ee7d02d1b94b9ca61cc6cb38ab8b9
Tien Dat <tiendatbinhan@gmail.com>
2024-01-12 23:14:43 +0700

    Fix some bugs

---
+++ app/src/main/java/com/nt118/uit21520707/jupita/PlaylistFragment.java
@@ -0,0 +1,24 @@
+package com.nt118.uit21520707.jupita;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PlaylistFragment extends Fragment {
+    public PlaylistFragment()
+    {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_playlist, container, false);
+        return view;
+    }
+}
