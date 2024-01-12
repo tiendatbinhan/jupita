d0d7d333bf72e82f8f6083e3e84dc19384cd4306
Tien Dat <tiendatbinhan@gmail.com>
2024-01-12 22:37:58 +0700

    Fix some bugs

---
+++ app/src/main/java/com/nt118/uit21520707/jupita/HomepageActivity.java
@@ -0,0 +1,13 @@
+package com.nt118.uit21520707.jupita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomepageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
